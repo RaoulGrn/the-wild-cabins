f16da5e8956e532ce50d802eb115406657972ce0
Raoul <grnraoul@gmail.com>
2024-06-09 13:32:45 +0300

    adding fonts, styled components setup and Button, Input and StyledApp customization

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import { useState } from "react";
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: goldenrod;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Cabins</H1>
+        <Button onClick={() => alert("Check in")}>Check in</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+
+        <Input type="number" placeholder="Number of guests" />
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
