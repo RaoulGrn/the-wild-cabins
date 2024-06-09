28cd2e10f0abca4cb9e6a03921b810fe3188cca7
Raoul <grnraoul@gmail.com>
2024-06-09 15:37:06 +0300

    custom row element and refactoring button element to include sizes and variations

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: orangered;
   padding: 20px;
 `;
 
@@ -15,15 +15,30 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading as="h1">The Wild Cabins</Heading>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Cabins</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Check in")}>Check in</Button>
+              <Button
+                variation="danger"
+                size="medium"
+                onClick={() => alert("Check out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Heading as="h2">Check in and out</Heading>
-        <Button onClick={() => alert("Check in")}>Check in</Button>
-        <Button onClick={() => alert("Check out")}>Check out</Button>
-
-        <Heading as="h3">Forms</Heading>
-        <Input type="number" placeholder="Number of guests" />
-        <Input type="number" placeholder="Number of guests" />
+          <Row type="vertical">
+            <form>
+              <Heading as="h3">Forms</Heading>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
